686bce5d5c92d43bca089b44d5d5bede72c6c027
Fikayo Ebyn-Ahisu <oluwafikayo07@gmail.com>
2026-04-30 14:04:14 -0600

    Nav bar done

---
+++ index.js
@@ -0,0 +1,15 @@
+import  express  from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req,res) => {
+    res.render("index.ejs")
+});
+
+app.listen(3000, () => {
+    console.log("listening on port 3000")
+});
